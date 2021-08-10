af3d9bbc5abc079b348d4d3f9d8b0eb8d53c2eb9
MinhDinh5 <minhd5@nycstudents.net>
2021-08-09 22:59:08 -0400

    Busy day

--- JavaScript review.js
+++ JavaScript review.js
@@ -281,3 +281,32 @@
   greenEnergy(spaceship);
   remotelyDisable(spaceship);
   console.log(spaceship);
+
+//Looping through objects
+  let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+  }; 
+
+  for (let n in spaceship.crew){
+  console.log(`${spaceship.crew[n].name}: ${spaceship.crew[n].degree}`);
+  }
