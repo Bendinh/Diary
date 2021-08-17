347355a96d480072ed6ae1f23d20a32f71bc23f7
MinhDinh5 <minhd5@nycstudents.net>
2021-08-16 20:48:34 -0400

    Advanced Objects

--- JavaScript review.js
+++ JavaScript review.js
@@ -321,3 +321,83 @@
     }
   };
   console.log(robot.provideInfo());
+  ///////Don't use arrow fucntions while using this keyword
+
+//Getters for JavaScript
+  const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    get energyLevel(){
+      if (typeof this._energyLevel === 'number')
+      {
+        return `My current energy level is ${this._energyLevel}`
+      } else {
+        return 'System malfunction: cannot retrieve energy level'
+      }
+    }
+  };
+  console.log(robot.energyLevel);
+
+//Setter for JavaScript
+  const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    _numOfSensors: 15,
+    get numOfSensors(){
+      if(typeof this._numOfSensors === 'number'){
+        return this._numOfSensors;
+      } else {
+        return 'Sensors are currently down.'
+      }
+    },
+    set numOfSensors(num){
+      if(typeof num === 'number'&&num>=0){
+        this._numOfSensors = num;
+      } else {
+        console.log('Pass in a number that is greater than or equal to 0');
+      }
+    }
+  };
+  robot.numOfSensors = 100;
+  console.log(robot.numOfSensors);
+
+//Object Factory(Mass produce objects)
+  const robotFactory = (model, mobile) => {
+    return {
+      model: model,
+      mobile: mobile,
+      beep(){ 
+        console.log('Beep Boop')
+      }
+    }
+  }
+  const tinCan = robotFactory('P-500',true);  
+  tinCan.beep();
+  //Truncated version
+  function robotFactory(model, mobile){
+    return {
+      model,
+      mobile,
+      beep() {
+        console.log('Beep Boop');
+      }
+    }
+  }
+
+//Shortcut when accessing properties inside object to assign to a const
+  const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+      beep() {
+        console.log('Beep Boop');
+      },
+      fireLaser() {
+        console.log('Pew Pew');
+      },
+    }
+  };
+  
+  const {functionality} = robot;
+  functionality.beep();
+
